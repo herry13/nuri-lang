ca74cac5b189bf0e8d91b114cf43e793921bbf39
Herry <herry13@gmail.com>
2014-12-29 23:17:24 +0000

    rename eval -> check_type_of; tidy-up codes

--- ocaml/typechecker.mli
+++ ocaml/typechecker.mli
@@ -1,39 +1,16 @@
-(* Author: Herry (herry13@gmail.com) *)
+(** Module Typechecker contains the type-checking function.
 
-open Common
+    Module dependencies:
+    - Common
+    - Syntax
+    - Type
+    - Domain
 
-(*******************************************************************
- * type environment
- *******************************************************************)
+    @author Herry (herry13\@gmail.com)
+    @since 2014
+*)
 
-type map = Syntax.t MapRef.t
+open Syntax
+open Type
 
-(*******************************************************************
- * functions of type environment
- *******************************************************************)
-
-exception TypeError of int * string
-
-val string_of_map : map -> string
-
-val nuriSpecification : ?main:string list -> Syntax.nuri -> map
-
-val type_of : Domain.reference -> map -> Syntax.t
-
-val subtype : Syntax.t -> Syntax.t -> bool
-
-
-(*******************************************************************
- * a map from type to set of values
- *******************************************************************)
-
-module MapType : Map.S with type key = Syntax.t
-
-type type_values = Domain.SetValue.t MapType.t
-
-val values_of : Syntax.t -> type_values -> Domain.SetValue.t
-
-val add_value : Syntax.t -> Domain.value -> type_values -> type_values
-
-val make_type_values : map -> Domain.flatstore -> map ->
-	Domain.flatstore -> Domain.SetValue.t MapType.t
+val check_type_of : ?main:string list -> nuri -> map
